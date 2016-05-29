e698987d3e1a483a702a6dfc4a775a846e3e1da6
ankit singh <ankit.singh@ubona.com>
2016-05-29 16:04:29 +0530

    final 95

--- create.bat
+++ create.bat
@@ -2,7 +2,7 @@ set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_17\
 set PATH==%JAVA_HOME%\bin;c:\Users\AnkitSingh\Downloads\apache-ant-1.9.7-bin\apache-ant-1.9.7\bin;c:\mingw\bin;c:\MinGW\msys\1.0\bin;c:\Program Files (x86)\WiX Toolset v3.10
 
 set ANT_HOME=c:\Users\AnkitSingh\Downloads\apache-ant-1.9.7-bin\apache-ant-1.9.7
-set BZ2_HOME=c:\mingw
+set BZ2_HOME=E:\Movies\altel\src\native\windows\setup
 set LZMA_HOME=c:\mingw\xz
 set MINGW_HOME=c:\mingw
 start /B /LOW /WAIT ant build-installation-wix
